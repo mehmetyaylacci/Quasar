93b9512f02049eb2906e8278aee5e6e186f371ac
burakturksever <47771419+burakturksever@users.noreply.github.com>
2020-11-04 18:22:16 +0300

    Fixes

--- src/gui.py
+++ src/gui.py
@@ -2,15 +2,26 @@ import PySimpleGUI as sg
 import random
 from bs4_scraper import Scraper_bs as sc_bs
 
-BOX_SIZE = 80
 
+# constants
+PUZZLE_BOX = 130
+
+# fonts
+font_letter = ("Arial Black", 32)
+font_number = ("Courier New", 18)
+font_clues = ('Courier New', 16)
+
+# themes and colors
 sg.change_look_and_feel("NeutralBlue")
+
+
 
 layout = [
     [sg.Frame('',[
-        [sg.Graph((1280, 720), (0, 0), (0, 0), key='graph',
-            change_submits=True, drag_submits=False), sg.Text('', key='across', 
-            size=(30, 10)), sg.Text('', key='down', size=(30, 10))],
+        [sg.Graph(canvas_size=(1000, 720), graph_bottom_left=(0, 720), graph_top_right=(1000, 0),
+         key='graph', change_submits=True, drag_submits=False), sg.Text('', key='across', 
+            size=(30, 10), font=font_clues), sg.Text('', key='down', size=(30, 10),
+             font=font_clues)],
         [sg.Button('Clear'), sg.Button('Exit')]])
     ]
 ]
@@ -30,7 +41,7 @@ for clue in clues:
 print(across_string)
 print(down_string)
 
-sg.Input(justification='center',size=(100,1))
+sg.Input(justification='center', size=(100, 1))
 
 window = sg.Window('XOxygen Puzzle Solver', layout, finalize=True)
 
@@ -40,23 +51,25 @@ window.FindElement('down').update(down_string)
 g = window['graph']
 
 
-
-# font for letters
-font_letter = ("Courier New", 18)
-
 for row in range(5):
     for col in range(5):
+    
         if random.randint(0, 100) > 10:
-             g.draw_rectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE +
-                                        BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black', fill_color='white')
+            g.draw_rectangle((col * PUZZLE_BOX + 5, row * PUZZLE_BOX + 3),
+              (col * PUZZLE_BOX + PUZZLE_BOX + 5, row * PUZZLE_BOX + PUZZLE_BOX + 3),
+               line_color='black', fill_color='white')
+    
         else:
-            g.draw_rectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE +
-                                                                        5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black', fill_color='black')
-        g.draw_text('{}'.format(row * 5 + col + 1),
-                    (col * BOX_SIZE + 10, row * BOX_SIZE + 8))
-
-        g.draw_text('{}'.format('A'),
-                    (col * BOX_SIZE + 30, row * BOX_SIZE + 30), font=font_letter)
+            g.draw_rectangle((col * PUZZLE_BOX + 5, row * PUZZLE_BOX + 3),
+             (col * PUZZLE_BOX + PUZZLE_BOX + 5, row * PUZZLE_BOX + PUZZLE_BOX + 3),
+              line_color='black', fill_color='black')
+    
+        g.draw_text('{}'.format(row * 5 + col + 1), (col * PUZZLE_BOX + 20,
+        row * PUZZLE_BOX + 20),
+        font=font_number)
+        g.draw_text('{}'.format('A'), (col * PUZZLE_BOX + (PUZZLE_BOX/2),
+        row * PUZZLE_BOX + (PUZZLE_BOX/2)), 
+        font=font_letter)
 
 
 while True:
@@ -67,8 +80,8 @@ while True:
     if event in (None, "Clear"):
         for row in range(5):
             for col in range(5):
-                g.draw_rectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE +
-                                        BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black', fill_color='white')
+                g.draw_rectangle((col * PUZZLE_BOX + 5, row * PUZZLE_BOX + 3), (col * PUZZLE_BOX +
+                                        PUZZLE_BOX + 5, row * PUZZLE_BOX + PUZZLE_BOX + 3), line_color='black', fill_color='white')
     if event in (None, "Exit"):
         break
 window.close()
