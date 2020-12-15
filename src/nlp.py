09a715c0af07bd2c3c5fc25adb1015e580340895
Mehmet Yaylacı <mehmet.yaylaci@ug.bilkent.edu.tr>
2020-12-16 01:08:02 +0300

    #2

--- src/nlp.py
+++ src/nlp.py
@@ -1,4 +1,27 @@
+import nltk
+import gensim
+import gensim.downloader as api
+from nltk.tokenize import word_tokenize
+# from nltk.corpus import stopwords
+import string
 
-class NLP:
-    def __init__(self) -> None:
-        super().__init__()
+clue = "mehmet is sexy"
+word_vectors = api.load("glove-wiki-gigaword-50")
+
+stopwords = nltk.download('stopwords')
+
+
+stop = stopwords.words('english') + list(string.punctuation)
+guess_length = 10  # Length of word required for our crossword
+topn = 100  # No. of words to be returned by Gensim's most_similar()
+
+pos_words = [word for word in word_tokenize(clue.lower()) if word not in stop]
+
+probable_guesses = [word for word in word_vectors.most_similar(
+    positive=pos_words, topn=topn) if len(word[0]) == guess_length]
+
+print(probable_guesses)
+
+# class NLP:
+#     def __init__(self):
+#         //
