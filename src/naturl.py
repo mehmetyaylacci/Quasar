73cd0809360dc0f120684f347f669831d4cc2352
burakturksever <47771419+burakturksever@users.noreply.github.com>
2020-12-17 03:54:59 +0300

    finalized

--- src/naturl.py
+++ src/naturl.py
@@ -1,23 +1,44 @@
+# @authors:
+# Burak Turksever
+# Mehmet Yaylaci
+# Eralp Kumbasar
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk
 import json
+import requests
 from urllib import request
+from bs4 import BeautifulSoup
 import re
 from bs4_scraper import Scraper_bs as sc_bs
 import numpy as np
 import string
 
-
+'''
+NLP class uses datamuse API and sources to find possible answers
+that can be matched with the clues given in the crossword while
+obeying the restrictions and the layout of the puzzle
+'''
 class NLP:
+    '''
+    Uses the datamuse API to scrape possible answers corresponding
+    to the clues of the crossword puzzle
+    '''
     def __init__(self):
         self.starting_url = "https://api.datamuse.com/words?"
         self.url_adds = ["ml=", "rel_bga=", "rel_bgb="]
 
+    '''
+    determines possible answers for the crossword puzzle by searching for the
+    close neighbors of the clues in the network of datamuse search engine, the
+    words that may relate to the clues of the puzzle
+    '''
     def datamuse_close_neighs(self, clue, url_add):
         clue = clue.lower()
         clue = clue.replace(" ", "+")
         results = []
+
         lookup_url = self.starting_url + url_add + \
             clue + "&max=3"       # number can be changed
         response = request.urlopen(lookup_url)
@@ -26,22 +47,25 @@ class NLP:
             results.append(j["word"])
         return results
 
-    def purify_clues(self, clues):
+    '''
+    purifies the clues by filtering the stopwords and punctuations before searching
+    through the word engines to match the clues with possible answers for the puzzle
+    '''
+    def purify_clues(self, clues, lengths):
         purified_clues = []
         for a_clue in clues:
-            text = (a_clue[1].translate(str.maketrans(
-                '', '', string.punctuation))).lower()
+            text = (a_clue[1].translate(str.maketrans('', '', string.punctuation))).lower()
             tokenized = word_tokenize(text)
             with open("stop_words.txt", "r") as f:
                 stopwords = f.read().split("\n")
-                stopwords_removed = [
-                    word for word in tokenized if not word in stopwords]
+                stopwords_removed = [word for word in tokenized if not word in stopwords]
                 stringified = ""
                 for word in stopwords_removed:
                     stringified += word + " "
                 purified_clues.append(stringified)
         return purified_clues
 
+    # returns the clues and their lengths that are scraped from the puzzle
     def get_scraped_clues(self):
         scraper_obj = sc_bs()
         clues = scraper_obj.scrape_puzzle()
@@ -51,8 +75,8 @@ class NLP:
 
     '''
     the function returns the length of the clues given in the
-    puzzle according to the shape of the clues, creating a matrix of 
-    5x5 and by checking 
+    puzzle according to the shape of the clues, creating a matrix of
+    5x5 and by checking
     '''
     def clue_lengths(self, shape_clues, num_clues):
         rs = np.reshape(shape_clues, (5, 5))
@@ -70,12 +94,19 @@ class NLP:
             ct += 1
         return lens
 
+    '''
+    The function initiates guessing by scraping the clues,
+    storing the clues and creating guesses for the puzzle,
+    while also assigning weights according to different lengths.
+    it filters the initial list of guesses to obtain a final list.
+
+    '''
     def initiate_guessing(self):
         combo = self.get_scraped_clues()
         clues = combo[0]
         lens = combo[1]
         guesses = []
-        purified_clues = self.purify_clues(clues)
+        purified_clues = self.purify_clues(clues, lens)
         final_g = []
         ct = 0
         for i in purified_clues:
@@ -90,8 +121,8 @@ class NLP:
                     temp.append([purified_clues[ct], j + "s", 0.8])
                 elif len(j) > lens[ct]:
                     temp.append([purified_clues[ct], j[:lens[ct]], 0.5])
+                print(temp)
             final_g.append(temp)
-            ct += 1
 
         max_val = 0
         rtr = []
